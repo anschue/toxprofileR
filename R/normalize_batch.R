a7612a4321784843505dda4984a63cb18781222b
Andreas Schuettler <andreas.schuettler@ufz.de>
2018-07-20 10:57:57 +0200

    update documentation for normalize batch and adjust function so that it returns a list of EList objects

--- R/normalize_batch.R
+++ R/normalize_batch.R
@@ -1,14 +1,13 @@
-#' Title
+#' Normalize Batch
 #'
-#' @param dslist
-#' @param method
-#' @param output
+#' @param dslist list of EListRaw objects which should be normalized together
+#' @param method string, normalization method (default: "cyclicloess")
+#' @param output logical, if output plots should be given
 #'
-#' @return
+#' @return a list of EList objects containing the normalized data
 #' @export
 #'
-#' @examples
-normalizeBatch <- function(dslist, method = "cloess", output = T){
+normalizeBatch <- function(dslist, method = "cyclicloess", output = T){
 
   # check if probe names are in the same order---------------------------------
    lapply(1:(length(dslist)-1),function(id){
@@ -20,16 +19,20 @@ normalizeBatch <- function(dslist, method = "cloess", output = T){
   if(output){boxplot(log2(common.dataframe))}
 
   ##Normalize common dataframe
-  common.cloess<-limma::normalizeBetweenArrays(log2(common.dataframe),method="cyclicloess")
+  common.cloess<-limma::normalizeBetweenArrays(log2(common.dataframe),method=method)
+
+  if(output){boxplot(common.cloess)}
 
   ##Write normalized data into single dataframes
   dslist_norm <- lapply(1:length(dslist), function(id){
-    start_id <- if(id == 1){1}else{1+sum(unlist(lapply(1:(id-1),function(x){ncol(dslist[[x]][["E"]])})))}
+    start_id <- if(id == 1){1}else{1 + sum(unlist(lapply(1:(id-1), function(x){ncol(dslist[[x]][["E"]])})))}
     end_id <- sum(unlist(lapply(1:id,function(x){ncol(dslist[[x]][["E"]])})))
-    ds <- dslist[[id]]
+    ds <- new("EList", lapply(dslist[[id]],function(x){return(x)}))
     ds[["E"]] <- common.cloess[,start_id:end_id]
     return(ds)
   })
 
   return(dslist_norm)
 }
+
+#test_norm<-normalizeBatch(list(test1,test2))
