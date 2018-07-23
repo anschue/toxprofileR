bf6e950c8053a9cd456d6d88d183bdf1937a84a0
Andreas Schuettler <andreas.schuettler@ufz.de>
2018-07-23 10:57:14 +0200

    add function to calculate logFC

---
+++ R/calc_logfc.R
@@ -0,0 +1,19 @@
+#' Calculate logFC
+#'
+#' @param elist an EList with normalized expression values
+#'
+#' @return an EList with logFC values
+#' @export
+#'
+#' @examples
+calc_logfc <- function(elist){
+times<-unique(elist$targets$time_hpe)
+elist_logfc$E<-t(apply(elist$E,MARGIN = 1,FUN = function(gene){
+    gene_logfc<-gene
+    for(time in times){
+        gene_logfc[elist$targets$time_hpe==time]<-gene[elist$targets$time_hpe==time]-mean(gene[elist$targets$time_hpe==time&elist$targets$concentration_umol_l==0])
+    }
+    return(gene_logfc)
+}))
+
+}
