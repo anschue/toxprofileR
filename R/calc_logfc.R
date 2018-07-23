ab33dfa8c5790c8f0f58abe8ffe0a104ec98eea0
Andreas Schuettler <andreas.schuettler@ufz.de>
2018-07-23 12:17:01 +0200

    fix bug in calc_logfc.R and added progress bar function

--- R/calc_logfc.R
+++ R/calc_logfc.R
@@ -7,13 +7,14 @@
 #'
 #' @examples
 calc_logfc <- function(elist){
+elist_logfc <- elist
 times<-unique(elist$targets$time_hpe)
-elist_logfc$E<-t(apply(elist$E,MARGIN = 1,FUN = function(gene){
+elist_logfc$E<-t(pbapply::pbapply(elist$E,MARGIN = 1,FUN = function(gene){
     gene_logfc<-gene
     for(time in times){
         gene_logfc[elist$targets$time_hpe==time]<-gene[elist$targets$time_hpe==time]-mean(gene[elist$targets$time_hpe==time&elist$targets$concentration_umol_l==0])
     }
     return(gene_logfc)
 }))
-
+return(elist_logfc)
 }
