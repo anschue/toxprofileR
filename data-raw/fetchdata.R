eb93c729e6327d493129b30015339398e1d448b7
Andreas Schuettler <andreas.schuettler@ufz.de>
2018-07-24 12:15:28 +0200

    add meta-analysis data to sysdata

---
+++ data-raw/fetchdata.R
@@ -0,0 +1,5 @@
+load("./data-raw/Annotation/mapComplete.Rd")
+new_annotation <- map
+x <- load("./data-raw/publicdata/logFCframe_all.Rd")
+
+devtools::use_data(new_annotation,logFCframe_all,comparisons_merge,internal = T,overwrite = T)
