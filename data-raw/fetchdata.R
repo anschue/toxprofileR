83096a6c40a14d2db5f2bfc315c05dae17a45cff
Andreas Schuettler <andreas.schuettler@ufz.de>
2018-07-25 15:48:06 +0200

    add nodeframe to internal package data

--- data-raw/fetchdata.R
+++ data-raw/fetchdata.R
@@ -1,5 +1,8 @@
 load("./data-raw/Annotation/mapComplete.Rd")
 new_annotation <- map
+
 x <- load("./data-raw/publicdata/logFCframe_all.Rd")
 
-devtools::use_data(new_annotation, logFCframe_all, comparisons_merge, internal = T, overwrite = T)
+load("./data-raw/toxuniverse/universe_nodeframe.Rd")
+
+devtools::use_data(new_annotation, logFCframe_all, nodeframe, comparisons_merge, internal = T, overwrite = T)
