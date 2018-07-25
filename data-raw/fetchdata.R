3da1e0e349ae34f9a6caa2feb548b40033751f5b
Andreas Schuettler <andreas.schuettler@ufz.de>
2018-07-25 19:48:22 +0200

    annotate fetchdata.R

--- data-raw/fetchdata.R
+++ data-raw/fetchdata.R
@@ -1,8 +1,12 @@
+# new array annotation --------------------------------------------------------
 load("./data-raw/Annotation/mapComplete.Rd")
 new_annotation <- map
 
+# public toxicogenomic data ---------------------------------------------------
 x <- load("./data-raw/publicdata/logFCframe_all.Rd")
 
+# toxicogenomic universe assignment -------------------------------------------
 load("./data-raw/toxuniverse/universe_nodeframe.Rd")
 
+# add to internal data --------------------------------------------------------
 devtools::use_data(new_annotation, logFCframe_all, nodeframe, comparisons_merge, internal = T, overwrite = T)
