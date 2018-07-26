9ba8d447fc54ce5c818b91ab0ec15c8d23bde18e
Andreas Schuettler <andreas.schuettler@ufz.de>
2018-07-26 17:07:27 +0200

    add function to fit model parameters to toxicogenomic profile

---
+++ R/fit_tcta.R
@@ -0,0 +1,87 @@
+#' Fit regression model on toxicogenomic profile
+#'
+#' @param elist An EList containing normalized logFC values from one exposure experiment.
+#' @param extrema A dataframe containing extreme values for each node
+#' @param cluster Logical, if modeling should be parallelized
+#'
+#' @return A dataframe with fitted model parameters for each node
+#' @export
+fit_tcta <- function(elist, extrema, cluster = c(TRUE, FALSE), cln = 2){
+    library("hydromad")
+    library("snow")
+    library("outliers")
+    library("mgcv")
+
+# create nodelist -------------------------------------------------------------
+nodelist <- toxprofileR::create_nodelist(elist)
+
+nodelist_extrema <- lapply(seq(1,length(nodelist)), function(nodeID){
+    if(is.data.frame(nodelist[[nodeID]])){
+    cbind(nodelist[[nodeID]], do.call("rbind", replicate(n = nrow(nodelist[[nodeID]]), expr = extrema[nodeID,],simplify = F)))
+    }else{return(NA)}
+})
+
+# set parameter boundaries ----------------------------------------------------
+conc_all<-elist$targets$concentration_umol_l[elist$targets$type!="recovery"]
+concentrations<-sort(unique(conc_all[conc_all!=0]), decreasing = T)
+dilution_factor<-concentrations[1]/concentrations[2]
+concrange<-max(concentrations)/min(concentrations)
+
+
+param_bounds <- list(slope = c(min = -log((1/0.505)-1)/log(concrange^(0.5)),
+                               max = -log((1/0.99)-1)/log(dilution_factor^0.5)),
+                     sigma = c(min = log(1.5)/((-2*log(0.01))^0.5),
+                               max = log(24)/((-2*log(0.99))^0.5)),
+                     EC50 = c(min = min(concentrations)/concrange,
+                              max = max(conc_all)*concrange),
+                     mS50 = c(min = 1/(max(conc_all)*concrange),
+                              max = 1/(min(concentrations)/concrange)),
+                     mu = c(min = 1.5,
+                            max = 75),
+                     mconc = c(min = min(concentrations)/concrange,
+                               max = max(conc_all)*concrange),
+                     sconc = c(min = log(dilution_factor)/((-2*log(0.01))^0.5),
+                               max = log(concrange)/((-2*log(0.99))^0.5)),
+                     err = c(min = 0,
+                             max = 10))
+
+
+if(cluster){
+# check for external cluster --------------------------------------------------
+cl<-snow::getMPIcluster()
+
+# if not available set up cluster ---------------------------------------------
+if(is.null(cl)){
+cl <- snow::makeMPIcluster(cln)
+}
+
+# load libraries on cluster ---------------------------------------------------
+snow::clusterEvalQ(cl = cl, expr = library("limma"))
+snow::clusterEvalQ(cl, expr = library("hydromad"))
+snow::clusterEvalQ(cl, expr = library("outliers"))
+snow::clusterEvalQ(cl, expr = library("mgcv"))
+snow::clusterEvalQ(cl, expr = library("toxprofileR"))
+
+# load data on cluster --------------------------------------------------------
+snow::clusterExport(cl, c("nodelist_extrema", "param_bounds"))
+
+# apply modeling function -----------------------------------------------------
+
+tcta_paramlist_som <- snow::parLapply(cl = cl, x = nodelist_extrema, fun = toxprofileR::get_tcta_params, param_bounds = param_bounds)
+
+snow::stopCluster(cl)
+} else {
+# apply modeling without paralellization
+pbapply::pblapply(X = nodelist_extrema,FUN = toxprofileR::get_tcta_params, param_bounds = param_bounds)
+}
+
+tcta_paramframe_som <- as.data.frame(do.call("rbind",tcta_paramlist_som))
+
+tcta_paramframe_som
+}
+
+
+
+
+
+
---
+++ R/get_tcta_params.R
@@ -0,0 +1,215 @@
+#' Get regression model parameters for one node
+#'
+#' @param nodeframe A dataframe containing logFC, time, concentration of a node
+#' @param param_bounds A list containing parameter boundaries for hill-gauss and gauss-gauss model
+#'
+#' @return Returns a named vector with fitted model parameters
+#' @export
+#'
+get_tcta_params<-function(nodeframe, param_bounds){
+
+    if(is.data.frame(nodeframe)){
+
+        nodeframe <- toxprofileR::remove_outliers(nodeframe)
+        nodeframe <- nodeframe[!is.na(nodeframe$logFC),]
+
+        # define regression models --------------------------------------------
+
+        ## Hill-Gauss ---------------------------
+        abund.funct<-function(dose,time,hillslope,maxS50,mu,sigma,maxGene){
+            maxGene/(1+exp(-hillslope*(log(dose)-log(1/((maxS50)*exp(-0.5*(((log(time)-log(mu))/sigma)^2)))))))}
+
+        ## Gauss-Gaus ---------------------------
+        abund.funct_gaugau<-function(dose,time,mconc,sconc,mu,sigma,maxGene){
+            maxGene*exp(-((((log(dose)-log(mconc))^2)/(2*(sconc^2)))+(((log(time)-log(mu))^2)/(2*(sigma^2)))))}
+
+
+        # define Likelihood functions -----------------------------------------
+
+        ## Hill-Gauss ---------------------------
+
+        ### upregulation --------------
+        Likelihood_theta_up<-function(theta){
+            sum(dnorm(x = nodeframe$logFC - abund.funct(dose = nodeframe$conc_all,time = nodeframe$timen_all, hillslope = theta[1],maxS50 = theta[2],mu = theta[3],sigma = theta[4],maxGene = nodeframe$max[1]),mean = 0,sd = theta[5],log = T))}
+
+        ### downregulation ------------
+        Likelihood_theta_down<-function(theta){
+            sum(dnorm(x = nodeframe$logFC - abund.funct(dose = nodeframe$conc_all, time = nodeframe$timen_all, hillslope = theta[1],maxS50 = theta[2],mu = theta[3],sigma = theta[4],maxGene = nodeframe$min[1]),mean = 0,sd = theta[5],log = T))}
+
+        ## Gauss-Gauss --------------------------
+
+        ### upregulation --------------
+        Likelihood_theta_up_gau<-function(theta){
+            sum(dnorm(x = nodeframe$logFC - abund.funct_gaugau(dose = nodeframe$conc_all, time = nodeframe$timen_all, mconc = theta[1],sconc = theta[2],mu = theta[3],sigma = theta[4],maxGene = nodeframe$max[1]),mean = 0,sd = theta[5],log = T))}
+
+        ### downregulation ------------
+        Likelihood_theta_down_gau<-function(theta){
+            sum(dnorm(x = nodeframe$logFC - abund.funct_gaugau(dose = nodeframe$conc_all, time = nodeframe$timen_all, mconc = theta[1],sconc = theta[2],mu = theta[3],sigma = theta[4],maxGene = nodeframe$min[1]),mean = 0,sd = theta[5],log = T))}
+
+
+        # define starting parameters ------------------------------------------
+
+        starthillslope <- 1
+        startmaxS50 <- 1/median(unique(nodeframe$conc_all[nodeframe$conc_all!=0]))
+        startmu <- median(unique(nodeframe$timen_all))
+        startsigma <- 0.3
+        starterr <- sd(nodeframe$logFC[nodeframe$conc_all==min(nodeframe$conc_all)&nodeframe$time_all==3])
+
+        startmconc<-nodeframe$conc_all[which.max(abs(nodeframe$logFC))]
+        startmu_gau<-nodeframe$timen_all[which.max(abs(nodeframe$logFC))]
+        startsconc<-0.3
+
+        initialg<-as.numeric(c(hillslope=starthillslope,maxS50 = startmaxS50,mu = startmu,sigma = startsigma,err=starterr))
+        names(initialg)<-c("hillslope","maxS50","mu","sigma","err")
+
+        initialg_gau<-as.numeric(c(mconc=startmconc,sconc = startsconc,mu = startmu_gau,sigma = startsigma,err=starterr))
+        names(initialg_gau)<-c("mconc","sconc","mu","sigma","err")
+
+        # Parameter estimation ------------------------------------------------
+
+        seeds <- c(1,12,123,1234,12345,123456)   # perfrom parameter estimation with different random seeds and afterwards select best fit
+
+        ## Hill-Gauss ----------------------------
+        ### up --------------
+        results_up_hill <- lapply(seq(1,length(seeds)), function(seedID){
+            set.seed(seeds[seedID])
+            hydromad::SCEoptim(FUN = Likelihood_theta_up,par = initialg,control=list(fnscale=-1,returnpop=F),lower=as.numeric(c(hillslope=param_bounds$slope["min"],maxS50=param_bounds$mS50["min"],mu=param_bounds$mu["min"],sigma=param_bounds$sigma["min"],err=param_bounds$err["min"])),upper=as.numeric(c(hillslope=param_bounds$slope["max"],maxS50=param_bounds$mS50["max"],mu=param_bounds$mu["max"],sigma=param_bounds$sigma["max"],err=param_bounds$err["max"])))
+        })
+
+        fit_up_hill <- results_up_hill[[which.min(unlist(lapply(results_up_hill, function(results){results$value})))]]
+
+        ### down ------------
+        results_down_hill <- lapply(seq(1,length(seeds)), function(seedID){
+            set.seed(seeds[seedID])
+            hydromad::SCEoptim(FUN = Likelihood_theta_down, par = initialg,control=list(fnscale=-1,returnpop=F),lower=as.numeric(c(hillslope=param_bounds$slope["min"],maxS50=param_bounds$mS50["min"],mu=param_bounds$mu["min"],sigma=param_bounds$sigma["min"],err=param_bounds$err["min"])),upper=as.numeric(c(hillslope=param_bounds$slope["max"],maxS50=param_bounds$mS50["max"],mu=param_bounds$mu["max"],sigma=param_bounds$sigma["max"],err=param_bounds$err["max"])))
+        })
+
+        fit_down_hill <- results_down_hill[[which.min(unlist(lapply(results_down_hill, function(results){results$value})))]]
+
+        ## Gauss-Gauss --------------------------
+        ### up --------------
+        results_up_gauss <- lapply(seq(1,length(seeds)), function(seedID){
+            set.seed(seeds[seedID])
+            hydromad::SCEoptim(FUN = Likelihood_theta_up_gau,par = initialg_gau,control=list(fnscale=-1,returnpop=F),lower=as.numeric(c(mconc=minmconc,sconc=minsconc,mu=param_bounds$mu["min"],sigma=param_bounds$sigma["min"],err=param_bounds$err["min"])),upper=as.numeric(c(mconc=maxmconc,sconc=maxsconc,mu=param_bounds$mu["max"],sigma=param_bounds$sigma["max"],err=param_bounds$err["max"])))
+        })
+
+        fit_up_gauss <- results_up_gauss[[which.min(unlist(lapply(results_up_gauss, function(results){results$value})))]]
+
+        ### down ------------
+        results_down_gauss <- lapply(seq(1,length(seeds)), function(seedID){
+            set.seed(seeds[seedID])
+            hydromad::SCEoptim(FUN = Likelihood_theta_down_gau,par = initialg_gau,control=list(fnscale=-1,returnpop=F),lower=as.numeric(c(mconc=minmconc,sconc=minsconc,mu=param_bounds$mu["min"],sigma=param_bounds$sigma["min"],err=param_bounds$err["min"])),upper=as.numeric(c(mconc=maxmconc,sconc=maxsconc,mu=param_bounds$mu["max"],sigma=param_bounds$sigma["max"],err=param_bounds$err["max"])))
+        })
+
+        fit_down_gauss <- results_down_gauss[[which.min(unlist(lapply(results_down_gauss, function(results){results$value})))]]
+
+
+        # retrieve AICs for model ----------------------------------------------
+
+        ## fit spline as "positive reference" ----------------------------------
+        nodeframe$ldose<-log(nodeframe$conc_all)
+        nodeframe$ltime<-log(nodeframe$timen_all)
+        what = mgcv::gam(formula = logFC~te(ldose,ltime,bs = "tp"),data=nodeframe[nodeframe$conc_all!=0,])
+
+        ## retrieve AICcs for Spline/Nullmodel as reference -------------------
+        n= nrow(nodeframe)
+        knull<-2
+        kfull<-5
+
+        AIC_spline<- -2*as.numeric(logLik(what))+(2*(attributes(logLik(what))$df+1)*n/(n-attributes(logLik(what))$df-2))
+        AIC_null<- -2*as.numeric(logLik(lm(nodeframe$logFC~1)))+(2*(knull+1)*n/(n-knull-2))
+
+        ## retrieve AICs for model fits ---------------------------------------
+        AIC_up_hill<- -2*as.numeric(-fit_up_hill$value)+(2*(kfull+1)*n/(n-kfull-2))
+        AIC_up_gauss<- -2*as.numeric(-fit_up_gauss$value)+(2*(kfull+1)*n/(n-kfull-2))
+        AIC_down_hill<- -2*as.numeric(-fit_down_hill$value)+(2*(kfull+1)*n/(n-kfull-2))
+        AIC_down_gauss<- -2*as.numeric(-fit_down_gauss$value)+(2*(kfull+1)*n/(n-kfull-2))
+
+        # compare AICs and calculate AICweights -------------------------------
+        sortframe<-data.frame(models=c(AIC_up_hill,AIC_null),index=c(1:2))
+        delta<-c(sortframe$models-min(sortframe$models,na.rm = T))
+        AICw_up_hill<-(exp(-delta/2)/sum(exp(-delta/2),na.rm = T))[1]
+        rm(list=c("sortframe","delta"))
+
+        sortframe<-data.frame(models=c(AICup_gauss,AIC_null),index=c(1:2))
+        delta<-c(sortframe$models-min(sortframe$models,na.rm = T))
+        AICw_up_gauss<-(exp(-delta/2)/sum(exp(-delta/2),na.rm = T))[1]
+        rm(list=c("sortframe","delta"))
+
+        sortframe<-data.frame(models=c(AIC_down_hill,AIC_null),index=c(1:2))
+        delta<-c(sortframe$models-min(sortframe$models,na.rm = T))
+        AICw_down_hill<-(exp(-delta/2)/sum(exp(-delta/2),na.rm = T))[1]
+        rm(list=c("sortframe","delta"))
+
+        sortframe<-data.frame(models=c(AIC_down_gauss,AIC_null),index=c(1:2))
+        delta<-c(sortframe$models-min(sortframe$models,na.rm = T))
+        AICw_down_gauss<-(exp(-delta/2)/sum(exp(-delta/2),na.rm = T))[1]
+        rm(list=c("sortframe","delta"))
+
+        sortframe<-data.frame(models=c(AIC_spline,min(AIC_up_hill,AIC_down_hill)),index=c(1:2))
+        delta<-c(sortframe$models-min(sortframe$models,na.rm = T))
+        AICw_vspline_hill<-(exp(-delta/2)/sum(exp(-delta/2),na.rm = T))[2]
+        rm(list=c("sortframe","delta"))
+
+        sortframe<-data.frame(models=c(AIC_spline,min(AIC_up_gauss,AIC_down_gauss)),index=c(1:2))
+        delta<-c(sortframe$models-min(sortframe$models,na.rm = T))
+        AICw_vspline_gauss<-(exp(-delta/2)/sum(exp(-delta/2),na.rm = T))[2]
+        rm(list=c("sortframe","delta"))
+
+        # summarize modeling results ------------------------------------------
+
+        finalmodelcoefs_up_hill<-fit_up_hill$par
+        finalmodelcoefs_up_gauss<-fit_up_gauss$par
+        finalmodelcoefs_down_hill<-fit_down_hill$par
+        finalmodelcoefs_down_gauss<-fit_down_gauss$par
+
+        finalmodelcoefs_best_hill<-unlist(list(c(fit_up_hill$par, max = nodeframe$max[1]),c(fit_down_hill$par, max = nodeframe$min[1]))[[which.max(c(AICw_up_hill,AICw_down_hill))]])
+        finalmodelcoefs_best_gauss<-unlist(list(c(fit_up_gauss$par, max = nodeframe$max[1]),c(fit_down_gauss$par, max = nodeframe$min[1]))[[which.max(c(AICw_up_gauss,AICw_down_gauss))]])
+
+        names(finalmodelcoefs_best_hill)<-paste0(names(finalmodelcoefs_best_hill),"_best_hill")
+        names(finalmodelcoefs_best_gauss)<-paste0(names(finalmodelcoefs_best_gauss),"_best_gauss")
+
+        names(finalmodelcoefs_up_hill)<-paste0(names(finalmodelcoefs_up_hill),"_up_hill")
+        names(finalmodelcoefs_down_hill)<-paste0(names(finalmodelcoefs_down_hill),"_down_hill")
+
+        names(finalmodelcoefs_up_gauss)<-paste0(names(finalmodelcoefs_up_gauss),"_up_gauss")
+        names(finalmodelcoefs_down_gauss)<-paste0(names(finalmodelcoefs_down_gauss),"_down_gauss")
+
+        finalmodelcoefs<-c(finalmodelcoefs_up_hill,
+                           finalmodelcoefs_down_hill,
+                           finalmodelcoefs_best_hill,
+                           finalmodelcoefs_up_gauss,
+                           finalmodelcoefs_down_gauss,
+                           finalmodelcoefs_best_gauss,
+                           AIC_spline=AIC_spline,
+                           AIC_null=AIC_null,
+                           AIC_up_hill=AIC_up_hill,
+                           AIC_down_hill=AIC_down_hill,
+                           AIC_up_gauss=AIC_up_gauss,
+                           AIC_down_gauss=AIC_down_gauss,
+                           AICw_up_hill=AICw_up_hill,
+                           AICw_up_gauss=AICw_up_gauss,
+                           AICw_down_hill=AICw_down_hill,
+                           AICw_down_gauss=AICw_down_gauss,
+                           AICw_vspline_hill = AICw_vspline_hill,
+                           AICw_vspline_gauss = AICw_vspline_gauss,
+                           Convergence_up_hill=fit_up_hill$convergence,
+                           Convergence_down_hill=fit_down_hill$convergence,
+                           Convergence_up_gauss=fit_up_gauss$convergence,
+                           Convergence_down_gauss=fit_down_gauss$convergence,
+                           maxGene_up=nodeframe$max[1],
+                           maxGene_down=nodeframe$min[1],
+                           maxGene_up_sub=max(aggregate(nodeframe$logFC,by=list(nodeframe$conc_all,nodeframe$time_all),median)[,"x"],na.rm = T),
+                           maxGene_down_sub=min(aggregate(nodeframe$logFC,by=list(nodeframe$conc_all,nodeframe$time_all),median)[,"x"],na.rm = T),
+                           direction_hill = c(1,-1)[which.max(c(AICw_up_hill, AICw_down_hill))],
+                           direction_gauss =  c(1,-1)[which.max(c(AICw_up_gauss, AICw_down_gauss))],
+                           best_model = c(1,1,2,2)[which.max(c(AICw_up_hill, AICw_down_hill,AICw_up_gauss, AICw_down_gauss))],
+                           best_model_dir = c(1,-1,1,-1)[which.max(c(AICw_up_hill, AICw_down_hill,AICw_up_gauss, AICw_down_gauss))])
+
+        if(max(c(AICw_up_hill, AICw_down_hill)) == max(c(AICw_up_gauss, AICw_down_gauss))){
+            finalmodelcoefs$best_model <- 0
+        }
+
+    return(finalmodelcoefs)
+    }else{NA}
+}
