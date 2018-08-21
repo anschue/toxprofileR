bf870c999d35e2a6ed09d3894284ab5a034cd281
Andreas Schuettler <andreas.schuettler@ufz.de>
2018-08-21 15:41:55 +0200

    make getBM_annotation work with ORF input

--- R/getBM_annotation.R
+++ R/getBM_annotation.R
@@ -14,112 +14,141 @@ getBM_annotation <-
 
         library("biomaRt")
 
-        BM_IDS <-
+            if (filter != "ensembl_gene_id"){
+        BM_ensembl <-
             biomaRt::getBM(
                 attributes = unique(
                     c(
                         filter,
-                        "ensembl_gene_id",
-                        "external_gene_name",
-                        "description",
-                        "hsapiens_homolog_associated_gene_name"
-                    )
+                        "ensembl_gene_id"
+                     )
                 ),
                 filters = filter,
                 values = values,
                 mart = mart,
                 uniqueRows = T
             )
-
+           
+           
+        values <- BM_ensembl$ensembl_gene_id
+        }
+        
+        
+        BM_IDS <-
+            biomaRt::getBM(
+                attributes =
+                    c(
+                        "ensembl_gene_id",
+                        "external_gene_name",
+                        "description",
+                        "hsapiens_homolog_associated_gene_name"
+                    ),
+                filters = "ensembl_gene_id",
+                values = values,
+                mart = mart,
+                uniqueRows = T
+            )
+        
         BM_IDS <-
             aggregate.data.frame(
                 BM_IDS,
-                by = list(BM_IDS[, filter]),
+                by = list(BM_IDS[, "ensembl_gene_id"]),
                 FUN = function(x) {
                     paste(unique(x))
                 }
             )[, -1]
-
+        
         BM_GO <-
             biomaRt::getBM(
                 attributes = c("ensembl_gene_id", "name_1006"),
-                filters = filter,
+                filters = "ensembl_gene_id",
                 values = values,
                 mart = mart,
                 uniqueRows = T
             )
-
+        
         BM_GO <-
             aggregate.data.frame(
                 BM_GO,
-                by = list(BM_GO[, filter]),
+                by = list(BM_GO[, "ensembl_gene_id"]),
                 FUN = function(x) {
                     paste(unique(x))
                 }
             )[, -1]
-
-
+        
+        
         BM_interpro <-
             biomaRt::getBM(
                 attributes = c("ensembl_gene_id", "interpro_description"),
-                filters = filter,
+                filters = "ensembl_gene_id",
                 values = values,
                 mart = mart,
                 uniqueRows = T
             )
-
+        
         BM_interpro <-
             aggregate.data.frame(
                 BM_interpro,
-                by = list(BM_interpro[, filter]),
+                by = list(BM_interpro[, "ensembl_gene_id"]),
                 FUN = function(x) {
                     paste(unique(x))
                 }
             )[, -1]
-
+        
         BM_phenotype <-
             biomaRt::getBM(
                 attributes = c("ensembl_gene_id", "phenotype_description"),
-                filters = filter,
+                filters = "ensembl_gene_id",
                 values = values,
                 mart = mart,
                 uniqueRows = T
             )
-
+        
         BM_phenotype <-
             aggregate.data.frame(
                 BM_phenotype,
-                by = list(BM_phenotype[, filter]),
+                by = list(BM_phenotype[, "ensembl_gene_id"]),
                 FUN = function(x) {
                     paste(unique(x))
                 }
             )[, -1]
-
+        
         BM_genetype <-
             biomaRt::getBM(
                 attributes = c("ensembl_gene_id", "gene_biotype"),
-                filters = filter,
+                filters = "ensembl_gene_id",
                 values = values,
                 mart = mart,
                 uniqueRows = T
             )
-
+        
         BM_genetype <-
             aggregate.data.frame(
                 BM_genetype,
-                by = list(BM_genetype[, filter]),
+                by = list(BM_genetype[, "ensembl_gene_id"]),
                 FUN = function(x) {
                     paste(unique(x))
                 }
             )[, -1]
-
-
+        
+        
         BM_all <-
             Reduce(
                 function(...)
-                    merge(..., by = filter, all = TRUE),
+                    merge(..., by = "ensembl_gene_id", all = TRUE),
                 list(BM_IDS, BM_GO, BM_interpro, BM_genetype, BM_phenotype)
             )
-
+            
+         if (filter != "ensembl_gene_id"){
+			 BM_all <- merge(BM_ensembl, BM_all, by = "ensembl_gene_id", all = TRUE)
+			 BM_all <- aggregate.data.frame(
+			 BM_all,
+			 by = list(BM_all[,filter]),
+			  FUN = function(x) {
+                    paste(unique(x))
+                }
+                )[, -1]
+			 }
+        
         return(BM_all)
     }
