5cde1057670b9448151ab6d750a7c1eb10b9fc20
Andreas Schuettler <andreas.schuettler@ufz.de>
2018-08-28 16:56:55 +0200

    update public logFC matrix

--- data-raw/fetchdata.R
+++ data-raw/fetchdata.R
@@ -4,7 +4,7 @@ load("./data-raw/Annotation/mapComplete.Rd")
 new_annotation <- map
 
 # public toxicogenomic data ---------------------------------------------------
-x <- load("./data-raw/publicdata/logFCframe_all.Rd")
+x <- load("./data-raw/publicdata/logFC_frame.Rd")
 
 # toxicogenomic universe assignment -------------------------------------------
 load("./data-raw/toxuniverse/universe_nodeframe.Rd")
@@ -13,4 +13,4 @@ load("./data-raw/toxuniverse/universe_nodeframe.Rd")
 load("./data-raw/toxuniverse/universe_grid.Rd")
 
 # add to internal data --------------------------------------------------------
-devtools::use_data(new_annotation, logFCframe_all, nodeframe, comparisons_merge, grid, internal = T, overwrite = T)
+devtools::use_data(new_annotation, logFC_frame, nodeframe, comparisons_merge, grid, internal = T, overwrite = T)
