c041c8c4bd0313d419fdee256b6eac9f45984aa2
Andreas Schuettler <andreas.schuettler@ufz.de>
2018-08-29 15:21:23 +0200

    improve outlier detection

--- R/import_data.R
+++ R/import_data.R
@@ -31,7 +31,7 @@ importData <-
              removeOutliers = T) {
         # read target file --------------------------------------------------------
         targets <- limma::readTargets(file = targetfile, sep = "\t")
-        targets <- targets[targets$scan_ID == scanID, ]
+        targets <- targets[targets$scan_ID == scanID,]
 
         # create unique names based on concentration and time ---------------------
         targets$names <-
@@ -81,30 +81,68 @@ importData <-
         # outlier detection --------------------------------------------------------
         include <- c(1:nrow(targets))
 
+        # KS statistics
         outliermetrics1 <-
             arrayQualityMetrics::outliers(log2(raw$E), method = "KS")
-        outliermetrics2 <-
-            arrayQualityMetrics::outliers(log2(raw$E), method = "sum")
-        outliermetrics3 <-
-            arrayQualityMetrics::outliers(log2(raw$E), method = "upperquartile")
 
-        exclude <- table(c(
+        # Expression sums
+        sums <- colSums(log2(raw$E), na.rm = T)
+        th_up <-
+            (quantile(sums, 0.75, na.rm = T) + 1.5 * IQR(sums, na.rm = T))
+        th_down <-
+            (quantile(sums, 0.25, na.rm = T) - 1.5 * IQR(sums, na.rm = T))
+        outliermetrics2 <-
+            list(
+                threshold = c(th_up, th_down),
+                which = which(sums > th_up | sums < th_down)
+            )
+
+        # Quantiles
+        qs <-
+            apply(log2(raw$E),
+                  2,
+                  quantile,
+                  na.rm = TRUE,
+                  probs = c(0.25, 0.75))
+        th_up <-
+            (quantile(qs[2, ], 0.75, na.rm = T) + 1.5 * IQR(qs[2, ], na.rm = T))
+        th_down <-
+            (quantile(qs[1, ], 0.25, na.rm = T) - 1.5 * IQR(qs[1, ], na.rm = T))
+        outliermetrics3 <-
+            list(
+                threshold = c(th_up, th_down),
+                which = which(qs[2, ] > th_up | qs[1, ] < th_down)
+            )
+
+        # Euclidean distance
+        MDS_out <-
+            limma::plotMDS(log2(raw$E), top = (nrow(raw$E) / 5), plot = F)
+        distances <- apply(MDS_out$distance.matrix, 2, mean)
+        outliermetrics4 <-
+            arrayQualityMetrics::boxplotOutliers(distances, 1)
+
+
+        exclude_tab <- table(c(
             as.numeric(outliermetrics1@which),
-            as.numeric(outliermetrics2@which),
-            as.numeric(outliermetrics3@which)
+            as.numeric(outliermetrics2$which),
+            as.numeric(outliermetrics3$which),
+            as.numeric(outliermetrics4$which)
         ))
 
         # exclude array if 2 of the tests detect it as outlier
-        exclude <- as.numeric(names(exclude)[exclude >= 2])
+        exclude <- as.numeric(names(exclude_tab)[exclude_tab >= 2])
         include <- include[!include %in% exclude]
 
+        if(length(exclude)>0){
         message(paste("detected", targets$names[exclude], "as outlier\n"))
+        }
 
         group <- rep("include", nrow(targets))
         group[exclude] <- "exclude"
 
         # plots ---------------------------------------------------------------
         if (output) {
+            # density plot
             limma::plotDensities((log2(raw$E)),
                                  legend = T,
                                  group = group,
@@ -112,7 +150,22 @@ importData <-
                                  main = targets$substance[1]
             )
 
-            boxplot((log2(raw$E)), col = (as.numeric(as.factor(group)) + 2), main = targets$substance[1])
+            # boxplot
+            par(mar = c(12, 3, 2, 1))
+            boxplot(
+                log2(raw$E),
+                col = (as.numeric(as.factor(group)) + 2),
+                main = targets$substance[1],
+                las = 2
+            )
+            par(mar = c(5.1, 4.1, 4.1, 1))
+
+            # MDS plot
+            limma::plotMDS(
+                log2(raw$E),
+                labels = raw$targets$names,
+                col = as.numeric(as.factor(group)) + 2
+            )
 
 
         }
