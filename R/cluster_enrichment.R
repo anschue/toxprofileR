27bd1031271b55eb0e5ddb111abef8291009a488
Andreas Schuettler <andreas.schuettler@ufz.de>
2018-11-23 15:46:39 +0100

    add function for functional enrichment of cluster assignments

---
+++ R/cluster_enrichment.R
@@ -0,0 +1,117 @@
+#' Functional enrichment of universe clusters
+#'
+#' @param cluster_table a dataframe with the columns 'ensembl' and 'clustername'
+#'
+#' @return returns a list with functional enrichments for ZFIN, interpro, GO, and reactome annotations
+#' @export
+#'
+enrich_clusters <- function(cluster_table){
+
+    if(requireNamespace("clusterProfiler", quietly = TRUE)){
+    ensembl_universe <- as.character(unique(cluster_table$ensembl))
+
+    cluster_enrichments <- pbapply::pblapply(unique(cluster_table$clustername), function(cluster){
+
+        genes_ensembl <- as.character(unique(cluster_table$ensembl[cluster_table$clustername==cluster]))
+
+        ezfin <- clusterProfiler::enricher(gene = genes_ensembl,
+                                           universe = ensembl_universe,
+                                           TERM2GENE = enrichment_terms$ZFIN$term2gene,
+                                           TERM2NAME = enrichment_terms$ZFIN$term2name,
+                                           pvalueCutoff = 0.05,
+                                           pAdjustMethod = "BH",
+                                           minGSSize = 10,
+                                           maxGSSize = 500)
+
+        einterpro <- clusterProfiler::enricher(gene = genes_ensembl,
+                                               universe = ensembl_universe,
+                                               TERM2GENE = enrichment_terms$interpro$term2gene,
+                                               TERM2NAME = enrichment_terms$interpro$term2name,
+                                               pvalueCutoff = 0.05,
+                                               pAdjustMethod = "BH",
+                                               minGSSize = 10,
+                                               maxGSSize = 500)
+
+        ereactome <- clusterProfiler::enricher(gene = genes_ensembl,
+                                               universe = ensembl_universe,
+                                               TERM2GENE = enrichment_terms$reactome$term2gene,
+                                               TERM2NAME = enrichment_terms$reactome$term2name,
+                                               pvalueCutoff = 0.05,
+                                               pAdjustMethod = "BH",
+                                               minGSSize = 10,
+                                               maxGSSize = 500)
+
+        ego <- clusterProfiler::enrichGO(gene = genes_ensembl,
+                                         universe = ensembl_universe,
+                                         OrgDb         = "org.Dr.eg.db",
+                                         keyType       = 'ENSEMBL',
+                                         ont           = "all",
+                                         pAdjustMethod = "BH",
+                                         pvalueCutoff  = 0.01,
+                                         qvalueCutoff  = 0.05,
+                                         readable = T,
+                                         pool = T)
+
+        results <- list(clustername = cluster, zfin = ezfin, interpro = einterpro, reactome = ereactome, GO = ego)
+        return(results)
+    })
+
+    ## ZFIN
+    zfin_enrichments <- lapply(cluster_enrichments, function(cluster_enrichment){
+        results <- cluster_enrichment$zfin@result
+        results$genenr_annot <- unlist(lapply(strsplit(results$GeneRatio, split = "/", fixed = T), function(x){as.numeric(x[2])}))
+        results$genenr_total <- length(cluster_enrichment$zfin@gene)
+        results$clustername <- cluster_enrichment$clustername
+        return(results)
+    })
+
+    zfin_enrichments <- do.call("rbind", zfin_enrichments)
+
+    zfin_enrichments$ratio_annot <- zfin_enrichments$Count/zfin_enrichments$genenr_annot
+    zfin_enrichments$ratio_total <- zfin_enrichments$Count/zfin_enrichments$genenr_total
+
+    ## GO
+    GO_enrichments <- lapply(cluster_enrichments, function(cluster_enrichment){
+        results <- cluster_enrichment$GO@result
+        results$genenr_annot <- unlist(lapply(strsplit(results$GeneRatio, split = "/", fixed = T), function(x){as.numeric(x[2])}))
+        results$genenr_total <- length(cluster_enrichment$GO@gene)
+        results$clustername <- cluster_enrichment$clustername
+        return(results)
+    })
+
+    GO_enrichments <- do.call("rbind", GO_enrichments)
+    GO_enrichments$ratio_annot <- GO_enrichments$Count/GO_enrichments$genenr_annot
+    GO_enrichments$ratio_total <- GO_enrichments$Count/GO_enrichments$genenr_total
+
+    ## Interpro
+    interpro_enrichments <- lapply(cluster_enrichments, function(cluster_enrichment){
+        results <- cluster_enrichment$interpro@result
+        results$genenr_annot <- unlist(lapply(strsplit(results$GeneRatio, split = "/", fixed = T), function(x){as.numeric(x[2])}))
+        results$genenr_total <- length(cluster_enrichment$interpro@gene)
+        results$clustername <- cluster_enrichment$clustername
+        return(results)
+    })
+
+    interpro_enrichments <- do.call("rbind", interpro_enrichments)
+
+    interpro_enrichments$ratio_annot <- interpro_enrichments$Count/interpro_enrichments$genenr_annot
+    interpro_enrichments$ratio_total <- interpro_enrichments$Count/interpro_enrichments$genenr_total
+
+
+    ## reactome
+    reactome_enrichments <- lapply(cluster_enrichments, function(cluster_enrichment){
+        results <- cluster_enrichment$reactome@result
+        results$genenr_annot <- unlist(lapply(strsplit(results$GeneRatio, split = "/", fixed = T), function(x){as.numeric(x[2])}))
+        results$genenr_total <- length(cluster_enrichment$reactome@gene)
+        results$clustername <- cluster_enrichment$clustername
+        return(results)
+    })
+
+    reactome_enrichments <- do.call("rbind", reactome_enrichments)
+
+    reactome_enrichments$ratio_annot <- reactome_enrichments$Count/reactome_enrichments$genenr_annot
+    reactome_enrichments$ratio_total <- reactome_enrichments$Count/reactome_enrichments$genenr_total
+
+return(list(ZFIN = zfin_enrichments, interpro = interpro_enrichments, reactome = reactome_enrichments, GO = GO_enrichments))
+    } else {message("For this function you need to install the package clusterProfiler")}
+}
--- data-raw/fetchdata.R
+++ data-raw/fetchdata.R
@@ -1,6 +1,4 @@
 rm(list = ls())
-# new array annotation --------------------------------------------------------
-new_annotation <- readRDS("./data-raw/Annotation/GPL20686annotation.Rds")
 
 # toxicogenomic universe assignment -------------------------------------------
 load("./data-raw/toxuniverse/universe_nodeframe.Rd")
@@ -8,9 +6,51 @@ load("./data-raw/toxuniverse/universe_nodeframe.Rd")
 # toxicogenomic universe grid -------------------------------------------------
 load("./data-raw/toxuniverse/universe_grid.Rd")
 
+
+# new array annotation --------------------------------------------------------
+new_annotation <- readRDS("./data-raw/Annotation/GPL20686annotation.Rds")
+
+## Annotation for Enrichment
+mart <- biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL",
+                         dataset = "drerio_gene_ensembl",
+                         host = "jul2018.archive.ensembl.org")
+
+# ZFIN annotation (downloaded from zfin.org on 2018/11/22)
+ZFIN_terms <- read.table(file="./data-raw/Annotation/wildtype-expression_fish_2018.11.20.txt", skip = 1, header=T, sep="\t", quote="")
+ZFIN_IDS <- biomaRt::getBM(attributes = c("zfin_id_id", "ensembl_gene_id"), values = as.character(ZFIN_terms$Gene.ID), filters = "zfin_id_id", mart = mart, uniqueRows = T)
+ZFIN_IDS <- unique(ZFIN_IDS, margin = 1)
+ZFIN_terms <- merge.data.frame(ZFIN_terms, ZFIN_IDS, by.x = "Gene.ID", by.y = "zfin_id_id", all=T)
+
+zfin_term2gene <- ZFIN_terms[, c("Super.Structure.ID", "ensembl_gene_id")]
+zfin_term2gene <- zfin_term2gene[!is.na(zfin_term2gene$Super.Structure.ID)&!is.na(zfin_term2gene$ensembl_gene_id),]
+zfin_term2name <- ZFIN_terms[, c("Super.Structure.ID", "Super.Structure.Name")]
+zfin_term2name <- unique(zfin_term2name[!is.na(zfin_term2name$Super.Structure.ID)&!is.na(zfin_term2name$Super.Structure.Name),], margin = 1)
+
+# Interpro annotation
+interpro_ids <- biomaRt::getBM(attributes = c("interpro","interpro_description", "ensembl_gene_id"), filters = "ensembl_gene_id", values = nodeframe$ensembl, mart = mart)
+interpro_term2gene <- unique(interpro_ids[,c("interpro","ensembl_gene_id")], margin = 1)
+interpro_term2name <- unique(interpro_ids[,c("interpro","interpro_description")], margin = 1)
+
+# Reactome annotation
+library("reactome.db")
+reactome_ids <- biomaRt::getBM(attributes = c("reactome", "ensembl_gene_id"), filters = "ensembl_gene_id", values = nodeframe$ensembl, mart = mart)
+reactome_term2gene <- unique(reactome_ids[,c("reactome","ensembl_gene_id")], margin = 1)
+reactome_term2name <- data.frame(reactome = unique(reactome_term2gene$reactome), name = NA, stringsAsFactors = F)
+reactome_term2name$name  <- unlist(lapply(reactome_term2name$reactome, function(r_id){
+    name <- reactomePATHID2NAME[[r_id]]
+    if(!is.null(name)){return(name)}else{return(NA)}
+}))
+reactome_term2name <- reactome_term2name[!is.na(reactome_term2name$name),]
+
+enrichment_terms <- list(ZFIN = list(term2gene = zfin_term2gene, term2name = zfin_term2name),
+                         interpro = list(term2gene = interpro_term2gene, term2name = interpro_term2name),
+                         reactome = list(term2gene = reactome_term2gene, term2name = reactome_term2name))
+
+
 # add to internal data --------------------------------------------------------
 devtools::use_data(
     new_annotation,
+    enrichment_terms,
     internal = T,
     overwrite = T
 )
